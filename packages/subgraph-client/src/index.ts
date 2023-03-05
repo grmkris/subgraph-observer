1cc494c8a2775df4e6ea45f02b3612e80b112c3e
grmkris <kristjan.grm1@gmail.com>
2023-03-06 00:19:29 +0100

    payload

--- packages/subgraph-client/src/index.ts
+++ packages/subgraph-client/src/index.ts
@@ -2,3 +2,5 @@ export {
   SubgraphMetadataDocument,
   getSdk as getSubgraphMetadataSdk,
 } from "./graphql/generated/generated";
+
+export { getSdk as getBackendApiSdk } from "./backend/generated/generated";
